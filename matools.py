cefd98569934784b6ac6bf6cb902b1f6fc6894f1
Rida LALI <rlali@crans.org>
2021-06-10 04:53:31 +0200

    Ajout des fonctions log, gamma et bigamma

---
+++ matools.py
@@ -0,0 +1,12 @@
+"""
+Module contenant les outils mathématiques qui vont être utilisés
+"""
+
+import numpy as np
+import scipy as sp
+
+
+log = np.log
+gamma = sp.special.gamma
+psi = sp.special.digamma
+
