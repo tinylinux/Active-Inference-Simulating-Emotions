ba10e73e783664d4cb3957be7222c07d471130ad
Rida LALI <rlali@crans.org>
2021-06-14 10:49:59 +0200

    add pi function to have distribution

--- free_energy.py
+++ free_energy.py
@@ -31,3 +31,25 @@ def G(A, s, C):
     W = 1/2 * (Ainv - A0inv)
     R = np.inner(np.dot(A, s), (np.log(np.dot(A, s)) - np.log(C)))
     return R + np.inner(H, s) - np.inner(np.dot(A, s), np.dot(W, s))
+
+def pi(A, s, C, pi):
+    """
+    Calcul du vecteur de politique pour le choix de l'action
+    
+    Input
+    -----
+        A : matrix
+            Matrice A
+        s : [list : vector]
+            Vecteur des états internes
+        C : vector
+            Vecteur des préférences sur les outcomes
+        pi : int
+            Nombre de politiques
+    
+    Output
+    ------
+        scalar
+            L'énergie libre espérée
+    """
+    return mt.softmax(np.array([-1 * G(A[i], s[i], C) for i in range(1, pi+1)]))
