e82315a224c9c1f210b4e892fa9b3cdd7c820bdc
Rida LALI <rlali@crans.org>
2021-06-14 15:24:59 +0200

    Création du fichier d'extraction de matrices

---
+++ matrice.py
@@ -0,0 +1,29 @@
+"""
+Fichier d'import des matrices pour le modèle génératif
+"""
+
+import numpy as np
+import matools as mt
+
+# Politiques
+P_START = 0
+P_VALENCE = 1
+P_AROUSAL = 2
+P_MOTIVATION = 3
+P_BELIEFS = 4
+P_CONTEXT = 5
+P_R_SAD = 6
+P_R_AFRAID = 7
+P_R_ANGRY = 8
+P_R_HAPPY = 9
+P_n = 10
+
+# Matrice de départ
+D = np.loadtxt("matrices/D.m")
+
+# Matrices de transition
+B = []
+B_emo = np.loadtxt("matrices/B_emo.m")
+for i in range(P_n):
+    Bc = np.loadtxt("matrices/B_" + str(i+1) + ".m")
+    B.append(np.matrix(mt.block_diag(B_emo, Bc)))
