858780aa0ffca8be67eb46761a2c5978fce7275a
Rida LALI <rlali@crans.org>
2021-06-14 09:39:15 +0200

    Add G function

---
+++ free_energy.py
@@ -0,0 +1,33 @@
+"""
+Fichier traitant de la partie minimisation de l'énergie libre
+Pour le calcul de G, on s'appuie sur l'Appendix D de l'article - A synthesis
+"""
+
+import numpy as np
+import scipy as sp
+import matools as mt
+
+def G(A, s, C):
+    """
+    Fonction permettant de calculer l'énergie libre espérée
+    
+    Input
+    -----
+        A : matrix
+            Matrice A
+        s : vector
+            Vecteur des états internes
+        C : vector
+            Vecteur des préférences sur les outcomes
+    
+    Output
+    ------
+        scalar
+            L'énergie libre espérée
+    """
+    H = -1 * np.diag(np.inner(A, np.log(A)))
+    Ainv = np.matrix(np.array(A, dtype='f') ** -1)
+    A0inv = np.matrix(np.array(mt.a0(A), dtype='f') ** -1)
+    W = 1/2 * (Ainv - A0inv)
+    R = np.inner(np.dot(A, s), (np.log(np.dot(A, s)) - np.log(C)))
+    return R + np.inner(H, s) - np.inner(np.dot(A, s), np.dot(W, s))
