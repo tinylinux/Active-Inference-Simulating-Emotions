5de508139702ff9bc90c3e9a4969bdb2e94efc25
Rida LALI <rlali@crans.org>
2021-06-22 16:22:24 +0200

    Réadaptation de gradient pour adapter les vecteurs colonnes en matrices

--- gradient.py
+++ gradient.py
@@ -8,7 +8,7 @@ import matools as mt
 def gradF_s(s_emo, s_res, o, B_r, B_e, A, D):
     """
     Calcul du gradient de F (énergie libre) selon s
-    
+
     Input
     ------
         s_emo : [list : vector]
@@ -25,46 +25,53 @@ def gradF_s(s_emo, s_res, o, B_r, B_e, A, D):
             Lien Etats internes et physiologiques
         D : vector * vector
             Etats initiaux
-    
+
     Output
     -------
         [list : vector * vector]
     """
-    
-    logA = mt.logA(A, s_emo, o)
-    logB_r = np.log(B_r)
-    logB_e = np.log(B_e)
 
-    T = []
+    logA = np.matrix(mt.logA(A, s_emo, o))
+    logB_r = np.matrix(np.log(B_r))
+    logB_e = np.matrix(np.log(B_e))
+
+    Emo = []
+    Res = []
     n = len(s_emo)
     t = len(o)
     ke, kr = np.shape(s_emo[0]), np.shape(s_res[0])
-    
+
     if n > 1:
-        v_emo = np.ones(ke) + np.log(s_emo[0]) + np.inner(o[0], logA) + np.inner(s[1], logB_e) + np.log(D[0])
-        v_res = np.ones(kr) + np.log(s_res[0]) + np.inner(s[1], logB_r) + np.log(D[1])
-        T.append((v_emo, v_res))
+        print(np.shape(o[0]), np.shape(logA), np.shape(np.transpose(logA)))
+        AS = mt.inner(np.matrix(o[0]), logA)
+        v_emo = np.ones(ke) + np.log(s_emo[0]) + AS + mt.inner(np.matrix(s_emo[1]), logB_e) + np.log(D[0])
+        v_res = np.ones(kr) + np.log(s_res[0]) + mt.inner(s_res[1], logB_r) + np.log(D[1])
+        Emo.append(v_emo)
+        Res.append(v_res)
     for i in range(1, min(n-1, t)):
-        v_emo = np.ones(ke) + np.log(s_emo[i]) + np.inner(o[i], logA) + np.inner(s_emo[i+1], logB_e) + np.dot(logB_e, s_emo[i-1])
-        v_res = np.ones(kr) + np.log(s_res[i]) + np.inner(s_res[i+1], logB_r) + np.dot(logB_r, s_res[i-1])
-        T.append((v_emo, v_res))
-    for i in range(t, n-1)
-        v_emo = np.ones(ke) + np.log(s_emo[i]) + np.inner(s_emo[i+1], logB_e) + np.dot(logB_e, s_emo[i-1])
-        v_res = np.ones(kr) + np.log(s_res[i]) + np.inner(s_res[i+1], logB_r) + np.dot(logB_r, s_res[i-1])
-        T.append((v_emo, v_res))
+        v_emo = np.ones(ke) + np.log(s_emo[i]) + mt.inner(o[i], logA) + mt.inner(s_emo[i+1], logB_e) + np.dot(logB_e, s_emo[i-1])
+        v_res = np.ones(kr) + np.log(s_res[i]) + mt.inner(s_res[i+1], logB_r) + np.dot(logB_r, s_res[i-1])
+        Emo.append(v_emo)
+        Res.append(v_res)
+    for i in range(t, n-1):
+        v_emo = np.ones(ke) + np.log(s_emo[i]) + mt.inner(s_emo[i+1], logB_e) + np.dot(logB_e, s_emo[i-1])
+        v_res = np.ones(kr) + np.log(s_res[i]) + mt.inner(s_res[i+1], logB_r) + np.dot(logB_r, s_res[i-1])
+        Emo.append(v_emo)
+        Res.append(v_res)
     if n > 1:
         v_emo = np.ones(ke) + np.log(s_emo[n-1]) + np.dot(logB_e, s_emo[n-2])
         v_res = np.ones(kr) + np.log(s_res[n-1]) + np.dot(logB_r, s_res[n-2])
         if t >= n:
-            v_emo += np.inner(o[n-1], logA)
-        T.append((v_emo, v_res))
-    return T
+            v_emo += mt.inner(o[n-1], logA)
+        Emo.append(v_emo)
+        Res.append(v_res)
+    return (Emo, Res)
 
 
 def gradF_v(s, o, B, A, D):
     """
     Calcul du gradient de l'énergie libre suivant la variable v
-    
+
     Input
     ------
         s : [list : vector * vector]
@@ -77,7 +84,7 @@ def gradF_v(s, o, B, A, D):
             Lien entre états et outcomes
         D : vector * vector
             Vecteurs initiaux
-    
+
     Output
     -------
         [list : vector * vector]
@@ -88,16 +95,16 @@ def gradF_v(s, o, B, A, D):
     for k in s:
         s_emo.append(k[0])
         s_res.append(k[1])
-    
-    (gse, gsr) = gradF_s(s_emo, s_res, o, B[0], B[1], A, D)
-    
+
+    (gse, gsr) = gradF_s(s_emo, s_res, o, B[1], B[0], A, D)
+
     L = []
     for i in range(len(s)):
         d_emo = np.diag(s_emo[i])
         d_res = np.diag(s_res[i])
-        c_emo = np.inner(s_emo[i], s_emo[i])
-        c_res = np.inner(s_res[i], s_res[i])
-        v_emo = np.dot((d_emo - c_emo), gse[i])
-        v_res = np.dot((d_res - d_res), gsr[i])
+        c_emo = mt.inner(s_emo[i], s_emo[i])
+        c_res = mt.inner(s_res[i], s_res[i])
+        v_emo = np.dot((d_emo - c_emo), np.transpose(gse[i]))
+        v_res = np.dot((d_res - d_res), np.transpose(gsr[i]))
         L.append((v_emo, v_res))
     return L
