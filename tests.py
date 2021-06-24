73a2c2c573a1fb4e7734d94fe432845f18645495
Rida LALI <rlali@crans.org>
2021-06-24 15:50:37 +0200

    Réecriture du fichier de tests

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ Fichier pour tester les algos
 
 import numpy as np
 from scipy.special import softmax
+import matools as mt
 
 A = np.loadtxt("matrices/A_3.m")
 B_e = np.loadtxt("matrices/B_emo.m")
@@ -13,22 +14,22 @@ C = np.loadtxt("matrices/C.m")
 
 import gradient as grd
 
-s0_emo = D[0:4]
-s0_pos = D[4:]
+s0_emo = softmax(D[0:4])
+s0_pos = softmax(D[4:])
 
 s = [(np.array(s0_emo), np.array(s0_pos)) for i in range(5)]
 
 o = [np.array(C) for i in range(3)]
 
-print("s : ", s)
 Gd = np.array(grd.gradF_v(s, o, (B_e, B_s), A, (s0_emo, s0_pos)))
-print("Gd : ", Gd)
-k = 0
-while np.linalg.norm(Gd) < 1:
+# print("Gd : ", Gd)
+k = 1
+while mt.norm(Gd.reshape((10))) > 0.001:
     for i in range(5):
-        s[i] = (s[i][0] + Gd[i][0], s[i][1] + Gd[i][1])
+        s[i] = (mt.addmatvec(s[i][0],-1/(2**k)*Gd[i][0]),
+                mt.addmatvec(s[i][1],-1/(2**k)*Gd[i][1]))
+        s[i] = (mt.softmax(20*s[i][0]), mt.softmax(20*s[i][1]))
     Gd = np.array(grd.gradF_v(s, o, (B_e, B_s), A, (s0_emo, s0_pos)))
     k += 1
-    print("k = ", k)
-    print("s : ", s)
-    print("Gd : ", Gd)
+    print(mt.norm(Gd.reshape((10))))
+    print(s)
