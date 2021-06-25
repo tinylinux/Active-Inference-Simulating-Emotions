ef14890897a4ee06a70a32b4b019444692a134fa
Rida LALI <rlali@crans.org>
2021-06-25 20:36:14 +0200

    Import matrices

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ import numpy as np
 from scipy.special import softmax
 import matools as mt
 import free_energy as fe
+
+A = []
+B_s = [np.loadtxt("matrices/B_1.m")]
+B_e = np.loadtxt("matrices/B_emo.m")
+for i in range(2,11):
+    A.append(np.loadtxt("matrices/A_" + str(i) + ".m"))
+    B.append(np.loadtxt("matrices/B_" + str(i) + ".m"))
+D = np.loadtxt("matrices/D.m")
+C = np.loadtxt("matrices/C.m")
