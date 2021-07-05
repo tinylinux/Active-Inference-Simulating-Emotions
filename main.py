3822e0ca6bfbbd94c05071dec7d9a711b70f6959
Rida LALI <rlali@crans.org>
2021-07-05 15:55:04 +0200

    Correct functions + Fix Policy + Add Display

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matools as mt
 import free_energy as fe
 import parameters as pm
 import outcomes as ot
+import display as dp
 
 A = []
 B_s = [np.loadtxt("matrices/B_1.m")]
@@ -29,29 +30,34 @@ s_choosen = np.zeros((pm.T, pm.N_states))
 ## Initialisation des états
 for j in range(pm.N_policy):
     for i in range(pm.T):
-        s[j,i] = softmax(D)
+        s[j,i] = D[:]
+        s[j,i,pm.N_states_emo] = 0
+        s[j,i,pm.N_states_emo + j] = 1
+        s[j,i,:] = softmax(s[j,i,:])
 
 o = ot.get_outcomes()
+dp.plot_states(np.transpose(o), "Observations")
 
 ## Descente de gradient pour chaque politique
 for j in range(pm.N_policy):
-    print("Politique n°", j, " : ")
     Gd = np.array(grd.gradF_v(s[j], o, (B_e, B_s[j+1]), A[j], (s0_emo, s0_pos)))
     k = 1
-    print(mt.norm(Gd.reshape((pm.T * 2))))
     while mt.norm(Gd.reshape((10))) > 0.001:
         for i in range(pm.T):
             s[j,i,0:4] = s[j,i,0:4] - 1/(2**k)*Gd[i][0]
             s[j,i,4:]  = s[j,i,4:]  - 1/(2**k)*Gd[i][1]
-            s[j,i,0:4] = mt.softmax(50*s[j,i,0:4])
-            s[j,i,4:]  = mt.softmax(50*s[j,i,4:])
+            s[j,i,0:4] = mt.softmax(20*k*s[j,i,0:4])
+            s[j,i,4:]  = mt.softmax(20*k*s[j,i,4:])
         Gd = np.array(
             grd.gradF_v(s[j], o, (B_e, B_s[j+1]), A[j], (s0_emo, s0_pos))
             )
         k += 1
-        print(mt.norm(Gd.reshape((pm.T * 2))))
 
 ## Choix de l'action
 for j in range(pm.T):
-    p = fe.choose(A, s[:,j,0:4], C)
+    p = fe.choose(A, s[:,j,0:4], softmax(C))
+    print(p)
     s_choosen[j,:] = s[p,j,:]
+
+print(s_choosen)
+dp.plot_states(np.transpose(s_choosen), "Internal states")
