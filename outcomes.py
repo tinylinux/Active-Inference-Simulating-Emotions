6af3b8419cd2f6c7ec6ec29bbd4c97e6cde91c09
Rida LALI <rlali@crans.org>
2021-07-05 16:18:31 +0200

    Add import outcomes function

--- outcomes.py
+++ outcomes.py
@@ -13,10 +13,17 @@ def get_outcomes():
     Fonction de récupération des observations
     pour la simulation des émotions.
 
+    Input
+    -----
+        matrice
+
     Output
     ------
         [list : vector]
             Listes de distribution sur les observations
     """
     # To define it clearly - here it is an example
-    return [np.array(C) for i in range(3)]
+    out = []
+    for i in range(3):
+        out.append(np.loadtxt("matrices/outcomes/" + str(i+1) + ".t"))
+    return out
