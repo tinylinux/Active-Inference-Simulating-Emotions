8feeda044d26185b75e471419db3ecde6bd81713
Rida LALI <rlali@crans.org>
2021-07-14 15:08:49 +0200

    gradient adjustements

--- gradient.py
+++ gradient.py
@@ -5,7 +5,7 @@ Fichier contenant la fonction de calcul du gradient de l'énergie libre
 import numpy as np
 import matools as mt
 
-def gradF_s(s_emo, s_res, o, B_r, B_e, A, D):
+def gradF_s(s_emo, o, B_r, B_e, A, D):
     """
     Calcul du gradient de F (énergie libre) selon s
 
@@ -13,14 +13,8 @@ def gradF_s(s_emo, s_res, o, B_r, B_e, A, D):
     ------
         s_emo : [list : vector]
             Etats internes - Emotions
-        s_res : [list : vector]
-            Etats internes - Ressentis
         o : [list : vector]
             Observations/ Etats physiologiques
-        B_r : matrix
-            Transitions entre émotions
-        B_e : matrix
-            Transitions entre actions
         A : matrix
             Lien Etats internes et physiologiques
         D : vector * vector
@@ -30,8 +24,8 @@ def gradF_s(s_emo, s_res, o, B_r, B_e, A, D):
     -------
         [list : vector * vector]
     """
-
-    logA = np.matrix(mt.logA(A, s_emo, o))
+    logA = np.matrix(np.log(A))
+    #logA = np.matrix(mt.logA(A, s_emo, o))
     logB_r = np.matrix(np.log(B_r))
     logB_e = np.matrix(np.log(B_e))
 
@@ -39,41 +33,29 @@ def gradF_s(s_emo, s_res, o, B_r, B_e, A, D):
     #Res = []
     n = len(s_emo)
     t = len(o)
-    ke, kr = np.shape(s_emo[0]), np.shape(s_res[0])
+    ke = np.shape(s_emo[0])
 
-    grad = np.zeros((n, ke[0] + kr[0]))
+    grad = np.zeros((n, ke[0]))
 
     if n > 1:
-        AS = mt.inner(np.matrix(o[0]), logA)
-        v_emo = np.ones(ke) + np.log(s_emo[0]) + AS + mt.inner(np.matrix(s_emo[1]), logB_e) + np.log(D[0])
-        v_res = np.ones(kr) + np.log(s_res[0]) + mt.inner(s_res[1], logB_r) + np.log(D[1])
-        #Emo.append(v_emo)
-        #Res.append(v_res)
-        grad[0,0:4] = v_emo
-        grad[0,4:] = v_res
+        v_emo = np.ones(ke) + np.log(s_emo[0]) + mt.xlogy(np.matrix(o[0]), A) + np.log(D[0])
+        print(np.matrix(o[0]))
+        print(A)
+        print(mt.xlogy(np.matrix(o[0]), A))
+        print("------")
+        grad[0,:] = np.array(v_emo)
     for i in range(1, min(n-1, t)):
-        v_emo = np.ones(ke) + np.log(s_emo[i]) + mt.inner(o[i], logA) + mt.inner(s_emo[i+1], logB_e) + np.dot(logB_e, s_emo[i-1])
-        v_res = np.ones(kr) + np.log(s_res[i]) + mt.inner(s_res[i+1], logB_r) + np.dot(logB_r, s_res[i-1])
-        #Emo.append(v_emo)
-        #Res.append(v_res)
-        grad[i,0:4] = v_emo
-        grad[i,4:] = v_res
+        v_emo = np.ones(ke) + np.log(s_emo[i]) + mt.xlogy(np.matrix(o[i]), A)
+        grad[i,:] = np.array(v_emo)
     for i in range(t, n-1):
-        v_emo = np.ones(ke) + np.log(s_emo[i]) + mt.inner(s_emo[i+1], logB_e) + np.dot(logB_e, s_emo[i-1])
-        v_res = np.ones(kr) + np.log(s_res[i]) + mt.inner(s_res[i+1], logB_r) + np.dot(logB_r, s_res[i-1])
-        #Emo.append(v_emo)
-        #Res.append(v_res)
-        grad[i,0:4] = v_emo
-        grad[i,4:] = v_res
+        v_emo = np.ones(ke) + np.log(s_emo[i])
+        grad[i,:] = np.array(v_emo)
     if n > 1:
-        v_emo = np.ones(ke) + np.log(s_emo[n-1]) + np.dot(logB_e, s_emo[n-2])
-        v_res = np.ones(kr) + np.log(s_res[n-1]) + np.dot(logB_r, s_res[n-2])
         if t >= n:
-            v_emo += mt.inner(o[n-1], logA)
-        #Emo.append(v_emo)
-        #Res.append(v_res)
-        grad[n-1,0:4] = v_emo
-        grad[n-1,4:] = v_res
+            v_emo = np.matrix(np.ones(ke) + np.log(s_emo[n-1])) + mt.xlogy(np.matrix(o[n-1]), A)
+        else:
+            v_emo = np.matrix(np.ones(ke) + np.log(s_emo[n-1]))
+        grad[n-1,:] = np.array(v_emo)
     return grad
 
 
@@ -105,7 +87,7 @@ def gradF_v(s, o, B, A, D):
         s_emo.append(k[0:4])
         s_res.append(k[4:])
 
-    f = gradF_s(s_emo, s_res, o, B[1], B[0], A, D)
+    f = gradF_s(s_emo, o, B[1], B[0], A, D)
 
     gse = f[:,0:4]
     gsr = f[:,4:]
@@ -113,11 +95,11 @@ def gradF_v(s, o, B, A, D):
     L = []
     for i in range(len(s)):
         d_emo = np.diag(s_emo[i])
-        d_res = np.diag(s_res[i])
+        #d_res = np.diag(s_res[i])
         c_emo = mt.inner(s_emo[i], s_emo[i])
-        c_res = mt.inner(s_res[i], s_res[i])
+        #c_res = mt.inner(s_res[i], s_res[i])
         v_emo = np.dot((d_emo - c_emo), np.transpose(gse[i]))
-        v_res = np.dot((d_res - d_res), np.transpose(gsr[i]))
-        L.append((np.array(v_emo).reshape(4), v_res))
+        #v_res = np.dot((d_res - c_res), np.transpose(gsr[i]))
+        L.append(np.array(v_emo).reshape(4))
 
     return L
