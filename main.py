4a08b886a2c4e52a0a0c187293a590418a7743d6
Rida LALI <rlali@crans.org>
2021-07-14 15:07:30 +0200

    generate observations + change grad pour les émotions

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ C = np.loadtxt("matrices/C.m")
 import gradient as grd
 
 s0_emo = softmax(D[0:4])
-s0_pos = softmax(D[4:])
+s0_pos = D[4:]
 
 s = np.zeros((pm.N_policy,pm.T,pm.N_states))
 s_choosen = np.zeros((pm.T, pm.N_states))
@@ -32,32 +32,37 @@ for j in range(pm.N_policy):
     for i in range(pm.T):
         s[j,i] = D[:]
         s[j,i,pm.N_states_emo] = 0
-        s[j,i,pm.N_states_emo + j] = 1
-        s[j,i,:] = softmax(s[j,i,:])
+        s[j,i,pm.N_states_emo + 1 + j] = 1
+        s[j,i,0:4] = softmax(s[j,i,0:4])
 
-o = ot.get_outcomes()
+o = ot.gen_random_outcomes()
 dp.plot_states(np.transpose(o), "Observations")
+O = np.transpose(o)
 
 ## Descente de gradient pour chaque politique
 for j in range(pm.N_policy):
     Gd = np.array(grd.gradF_v(s[j], o, (B_e, B_s[j+1]), A[j], (s0_emo, s0_pos)))
     k = 1
-    while mt.norm(Gd.reshape((10))) > 0.001:
+    sz = np.size(Gd)
+    while mt.norm(Gd.reshape((sz))) > 0.0000001:
+        print(mt.norm(Gd.reshape((sz))))
         for i in range(pm.T):
-            s[j,i,0:4] = s[j,i,0:4] - 1/(2**k)*Gd[i][0]
-            s[j,i,4:]  = s[j,i,4:]  - 1/(2**k)*Gd[i][1]
-            s[j,i,0:4] = mt.softmax(20*k*s[j,i,0:4])
-            s[j,i,4:]  = mt.softmax(20*k*s[j,i,4:])
+            s[j,i,0:4] = s[j,i,0:4] - Gd[i]
+            s[j,i,0:4] = mt.softmax(s[j,i,0:4])
         Gd = np.array(
             grd.gradF_v(s[j], o, (B_e, B_s[j+1]), A[j], (s0_emo, s0_pos))
             )
+        print(Gd)
+        print(mt.norm(Gd.reshape((sz))))
+        print("-----------------------------")
         k += 1
 
 ## Choix de l'action
+G = np.zeros((pm.T, pm.N_policy))
 for j in range(pm.T):
-    p = fe.choose(A, s[:,j,0:4], softmax(C))
-    print(p)
+    (p, q) = fe.choose(A, s[:,j,0:4], softmax(C))
     s_choosen[j,:] = s[p,j,:]
+    G[j,:] = np.array(q)
 
-print(s_choosen)
+dp.plot_states(np.transpose(G), "Free energy")
 dp.plot_states(np.transpose(s_choosen), "Internal states")
