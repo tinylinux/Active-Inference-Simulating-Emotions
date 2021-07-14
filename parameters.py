276ba68546d0028944d98f79912554ee2f963806
Rida LALI <rlali@crans.org>
2021-07-14 18:08:41 +0200

    Peaufiner les paramètres

--- parameters.py
+++ parameters.py
@@ -4,6 +4,25 @@ Fichier où sont definis les paramètres
 
 T = 5
 
+Policy = {
+    1:  [1,2],      # Attend to Valence
+    #2:  [3,4],      # Attend to Arousal
+    3:  [5,6],      # Attend to Motivation
+    #4:  [7,8],      # Attend to Beliefs
+    #5:  [9,10],     # Attend to Context
+    6:  [11, 12],   # Report Sad
+    #7:  [11, 12],   # Report Afraid
+    #8:  [11, 12],   # Report Angry
+    9:  [11, 12]    # Report Happy
+}
+
+Emotions = {
+    1:  6,  # Sad
+    #2:  7,  # Afraid
+    #3:  8,  # Angry
+    4:  9   # Happy
+}
+
 N_policy = 9
 N_states_emo = 4
 N_states_act = 10
