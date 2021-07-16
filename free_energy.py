26494a9038a3617099924aa465815207b194ae8f
Rida LALI <rlali@crans.org>
2021-07-16 17:49:59 +0200

    Re-Create free_energy module file

---
+++ free_energy.py
@@ -0,0 +1,9 @@
+"""
+Fichier traitant de la partie minimisation de l'énergie libre
+Pour le calcul de G, on s'appuie sur l'Appendix D de l'article - A synthesis
+"""
+
+import numpy as np
+import mathtools as mt
+import display as dp
+import time
