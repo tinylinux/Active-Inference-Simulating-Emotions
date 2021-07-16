ff4e659a3a841f9eaf69213eaadfcdf042bde15a
Rida LALI <rlali@crans.org>
2021-07-16 17:25:34 +0200

    Changement des paramètres (Diminution des états/obs, Changement des matrices)

--- parameters.py
+++ parameters.py
@@ -5,27 +5,27 @@ Fichier où sont definis les paramètres
 T = 5
 
 Policy = {
-    1:  [1,2],      # Attend to Valence
-    #2:  [3,4],      # Attend to Arousal
-    3:  [5,6],      # Attend to Motivation
-    #4:  [7,8],      # Attend to Beliefs
-    #5:  [9,10],     # Attend to Context
-    6:  [11, 12],   # Report Sad
-    #7:  [11, 12],   # Report Afraid
-    #8:  [11, 12],   # Report Angry
-    9:  [11, 12]    # Report Happy
+    2:  [1,2],      # Attend to Valence
+    #3:  [3,4],      # Attend to Arousal
+    4:  [3,4],      # Attend to Motivation
+    #5:  [7,8],      # Attend to Beliefs
+    #6:  [9,10],     # Attend to Context
+    7:  [5, 6],   # Report Sad
+    #8:  [11, 12],   # Report Afraid
+    #9:  [11, 12],   # Report Angry
+    10:  [5, 6]    # Report Happy
 }
 
 Emotions = {
-    1:  6,  # Sad
-    #2:  7,  # Afraid
-    #3:  8,  # Angry
-    4:  9   # Happy
+    1:  7,  # Sad
+    #2:  8,  # Afraid
+    #3:  9,  # Angry
+    4:  10   # Happy
 }
 
-N_policy = 9
-N_states_emo = 4
-N_states_act = 10
+N_policy = 4
+N_states_emo = 2
+N_states_act = 5
 N_states = N_states_emo + N_states_act
 
-N_outcomes = 13
+N_outcomes = 7
