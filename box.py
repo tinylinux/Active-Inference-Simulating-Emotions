b933f62097f3adb7dc44d1204d3ce3800407b5e3
Rida LALI <rlali@crans.org>
2021-08-01 16:15:01 +0200

    Add functions : update, set with class documentation

--- box.py
+++ box.py
@@ -51,11 +51,87 @@ class ActInf(object):
     """
     Classe d'inférence active pour la simulation d'émotions
     pour un individu
+
+    Attributes
+    ----------
+    time :          integer
+        Duration of the active inference
+    emotions :      dict (integer -> integer)
+        Link emotion and report state
+    N_states_emo :  integer
+        Number of emotionnal states
+    N_states_act :  integer
+        Number of attentional focus states
+    states :        matrix (integer * integer * integer -> float)
+        Distribution over states
+    policy :        dict (integer -> integer list)
+        Policy with list of observations associed to each policy
+    N_policy :      integer
+        Number of policies
+    obs :           observations
+        Observations for this entity
     """
 
-    def __init__(self, arg):
+    def __init__(self):
+        """
+        Initialization.
+        """
         super(ActInf, self).__init__()
-        self.arg = arg
+        self.time = 0
+        self.N_states_emo = 1
+        self.N_states_act = 1
+        self.N_states = 2
+        self.N_policy = 1
+        self.states = np.matrix([])
+
+    def set_policy(self, policy):
+        """
+        Set policies
+
+        Input
+        -----
+        policy :    dict (integer -> integer list)
+            Policy with observations index attached to this policy
+        """
+        self.policy = policy
+        self.N_policy = len(policy.keys())
+
+    def set_states(self, emotions, nacts, t, D):
+        """
+        Set states
+
+        Input
+        -----
+        emotions :  dict (integer -> integer)
+            Link emotion and report state
+        nacts :     integer
+            Number of attentional focus state
+        t :         integer
+            Duration of active inference process
+        D :         array
+            Array of initial states
+        """
+        self.emotions = emotions
+        self.N_states_emo = len(emotions.keys())
+        self.N_states_act = nacts
+        self.N_states = self.N_states_emo + self.N_states_act
+        self.time = t
+        self.states = np.matrix(np.zeros((self.N_policy, t, self.N_states)))
+        for i in range(self.N_policy):
+            for j in range(t):
+                self.states[i, j, :] = D[:]
 
     def upd_obs(self, obs):
-        self.obs = np.array([])
+        """
+        Update observations
+
+        Input
+        -----
+        obs :     observation
+            All observations done for a entity by a entity (itself or subject)
+        """
+        self.obs = obs
+        if obs.time > self.time:
+            S = np.matrix(np.zeros(self.N_policy, obs.time, self.N_states))
+            S[:, 0:self.time, :] = self.states[:, :, :]
+            self.time = T
