ed0fb74116bb2ea3239dcdb6569309d490fa6d7a
Rida LALI <rlali@crans.org>
2021-08-02 13:56:33 +0200

    Main class, AI added

--- main_class.py
+++ main_class.py
@@ -15,6 +15,16 @@ from box import Observation, ActInf
 (D, A, Af, B, B_emo, C) = mat.import_matrices()
 (O, Og) = mat.observ_gen()
 
-Obs = Observation(O, pm.Policy)
+Obs = Observation(O, pm.Policy, Og)
 Obs.set_labels(pm.Outcomes[1:])
-Obs.display_graph()
+#Obs.display_graph()
+
+Ai = ActInf()
+Ai.set_policy(pm.Policy)
+Ai.set_states(pm.Emotions, pm.N_states_act, pm.T, D)
+Ai.upd_obs(Obs)
+Ai.set_matrices(A, Af, B, B_emo, D)
+Ai.belief_updating()
+Ai.choose_policy()
+Ai.set_labels(pm.States[1:])
+Ai.display_graph()
